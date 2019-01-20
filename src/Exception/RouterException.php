77f04d46017d4748bb2e747d0ae9b166540c5275
frank <frank@qlimix.net>
2019-01-20 16:20:41 +0100

    just routing

--- src/Exception/RouterException.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Qlimix\HttpMiddleware\Router\Exception;
-
-use Exception;
-
-final class RouterException extends Exception
-{
-}
--- src/MiddlewareRouter.php
+++ src/MiddlewareRouter.php
@@ -1,16 +1,31 @@
 <?php declare(strict_types=1);
 
-namespace Qlimix\HttpMiddleware\Router;
+namespace Qlimix\HttpMiddleware;
 
-use Psr\Http\Message\RequestInterface;
-use Qlimix\HttpMiddleware\Router\Exception\RouteNotFoundException;
-use Qlimix\HttpMiddleware\Router\Exception\RouterException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Qlimix\Router\Exception\RouteNotFoundException;
+use Qlimix\Router\Exception\RouterException;
+use Qlimix\Router\RouterInterface;
 
-interface MiddlewareRouter
+final class MiddlewareRouter implements MiddlewareInterface
 {
+    /** @var RouterInterface */
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     /**
      * @throws RouteNotFoundException
      * @throws RouterException
      */
-    public function route(RequestInterface $request): RequestInterface;
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $this->router->route($request)->handle();
+    }
 }
